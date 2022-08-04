28708742a2951e893990c74d2f24aadb890a6fbb
Abishini <97487129+Abishini@users.noreply.github.com>
2022-08-04 14:06:26 -0400

    Add files via upload

---
+++ Learn With Giraffe/Inheritance/Chef.py
@@ -0,0 +1,10 @@
+class Chef:
+
+    def make_chicken(Self):
+        print("The chef makes a chicken")
+
+    def make_salad(Self):
+        print("The chef makes a salad")
+
+    def make_special_dish(Self):
+        print("The chef makes a bbq ribs")
---
+++ Learn With Giraffe/Inheritance/ChineseChef.py
@@ -0,0 +1,13 @@
+class ChineseChef:
+
+    def make_chicken(Self):
+        print("The chef makes a chicken")
+
+    def make_salad(Self):
+        print("The chef makes a salad")
+
+    def make_special_dish(Self):
+        print("The chef makes orange chicken")
+
+    def make_fried_rice(Self):
+        print("The chef makes fried rie")
